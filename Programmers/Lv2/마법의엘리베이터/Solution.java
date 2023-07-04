ea3a087f1609ef88f124e23c510e3b5f0bae5d30
KimJinung <KimJinung@outlook.com>
2023-07-05 00:44:38 +0900

    2023.07.05.
    더 효율적인 풀이로 수정

--- Programmers/Lv2/마법의엘리베이터/Solution.java
+++ Programmers/Lv2/마법의엘리베이터/Solution.java
@@ -1,25 +1,22 @@
 package Lv2.마법의엘리베이터;
 
-import java.util.Arrays;
 
 class Solution {
 
-    private int elevator(int index, int[] arr, int acc) {
-        if (index == 0) {
-            return Arrays.stream(arr).sum() + acc;
-        }
+    private int elevator(int storey) {
+        if (storey == 0) return 0;
+        if (storey < 10) return Math.min(storey, 11 - storey);
+        if (storey % 10 == 0) return elevator(storey / 10);
 
-        int[] nextArr = Arrays.copyOf(arr, arr.length);
-        int nextAcc = 10 - nextArr[index];
-        nextArr[index] = 0;
-        nextArr[index - 1]++;
-        index--;
-        return Math.min(elevator(index, arr, acc), elevator(index, nextArr, acc + nextAcc));
+        int remainder = storey % 10;
+
+        int up = 10 - remainder + elevator(storey + 10 - remainder);
+        int down = remainder + elevator(storey - remainder);
+
+        return Math.min(up, down);
     }
 
     public int solution(int storey) {
-        String strNum = "0" + storey;
-        int[] arr = strNum.chars().map(i -> i - '0').toArray();
-        return elevator(arr.length - 1, arr, 0);
+        return elevator(storey);
     }
 }
