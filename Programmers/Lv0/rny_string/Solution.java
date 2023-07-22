6229b9b3f132b65d9dc8e8c179b277f84cfbfd8e
KimJinung <KimJinung@outlook.com>
2023-07-22 19:02:17 +0900

    2023.07.22.

---
+++ Programmers/Lv0/rny_string/Solution.java
@@ -0,0 +1,7 @@
+package Lv0.rny_string;
+
+class Solution {
+    public String solution(String rny_string) {
+        return rny_string.replace("m", "rn");
+    }
+}
---
+++ Programmers/Lv0/홀짝구분하기/Solution.java
@@ -0,0 +1,13 @@
+package Lv0.홀짝구분하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) System.out.printf("%s is even", n);
+        else System.out.printf("%s is odd", n);
+    }
+}
